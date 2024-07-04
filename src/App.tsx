6b78f6a27a0fc917264c86715e345447a1c8cf2c
Sergey <sergsmile29@gmail.com>
2024-07-04 12:18:53 +0300

    dsfadf

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo porro in animi, natus dolorem nihil impedit
+      quibusdam! Nihil repellendus pariatur voluptatibus suscipit? Iusto sint quaerat, dolorem ullam consectetur quae
+      cumque.
+    </div>
+  );
 };
