33208698b9fa64344cc67e2dc0ef3864520844e2
Sergey <sergsmile29@gmail.com>
2024-07-15 16:14:06 +0300

    add smt to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 export const App = () => {
   return (
     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo porro in animi, natus dolorem nihil impedit
-      quibusdam! Nihil repellendus pariatur voluptatibus suscipit? Iusto sint quaerat, dolorem ullam consectetur quae
-      cumque.
+      Lorem ipsum dolor sit amet consectetur animi, natus dolorem nihil impedit quibusdam! Nihil repellendus pariatur
+      voluptatibus suscipit? Iusto sint quaerat, dolorem ullam consectetur quae cumque.
     </div>
   );
 };
